cdfd242f0f400cca53bb9785a6cebe3b7f961d56
llisowski6706 <67202830+llisowski6706@users.noreply.github.com>
2021-02-18 08:35:00 +0100

    Add files via upload

---
+++ Cwiczenia/Cwiczenia/Llista.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace Cwiczenia
+{
+    internal class Llista<int32> : IEnumerable<int>, IEnumerable
+    {
+        private const int initlenght = 100;
+        private int[] array = new int[initlenght];
+        private int lenght = 0;
+        
+        
+
+        public void Add(int args)
+        {
+            if (array.Length > lenght)
+            {
+                array[lenght++] = args;
+                return;
+            }
+            
+                int i = 0;
+                int[] temparray = new int[array.Length + initlenght];
+                foreach (int item in array)
+                {
+                    temparray[i] = item;
+                    i++;
+                }
+                array = temparray;
+            Add(args);
+        }
+        
+        public void Remove(int args)
+        {
+            bool found = false;
+            int index = 0;
+            foreach (int item in array)
+            {
+                if (!found && item.Equals(args))
+                {
+                    found = true;
+                }
+                if (found && index + 1 < lenght)
+                {
+                    array[index] = array[index + 1];
+                }
+                index++;
+            }
+            lenght--;
+            int i = (array.Length - lenght) / initlenght;
+            if (i >= 1)
+            {
+                
+                int[] temparray = new int[array.Length - initlenght];
+                for (int j = 0; j < array.Length - initlenght; j++)
+                {
+                    temparray[j] = array[j];
+                }
+                array = temparray;
+            }
+        }
+        public int Count()
+        {
+            return lenght;
+        }
+        public void AddRange(int[] args)
+        {
+            foreach (int item in args)
+            {
+                Add(item);
+            }
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < lenght; i++)
+            {
+                yield return array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
---
+++ Cwiczenia/Cwiczenia/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Cwiczenia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int a;
+            Llista<int> pesel = new Llista<int>();
+            int[] lol = { 1, 3, 5 };
+            int[] test = { 1, 2, 3 };
+            //pesel.Add(5);
+            //pesel.Add(1111111);
+            //pesel.Remove(5);
+            //a = pesel.Count();
+            //  pesel.AddRange(lol);
+            
+            for (int i = 0; i < 203; i++)
+             {
+                    pesel.Add(i);
+             }
+            foreach (int item in pesel)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            for (int i = 0; i < 102; i++)
+            {
+                pesel.Remove(i);
+            }
+            foreach (int item in pesel)
+            {
+                Console.WriteLine(item);
+            }
+
+            // Console.WriteLine(a);
+            // pesel.AddRange(test);
+            Console.WriteLine("------------------");
+            foreach (int item in pesel)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
+    
